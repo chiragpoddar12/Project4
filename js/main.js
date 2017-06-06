122d2c4b4a74863dd37a2553c32552b54a8715d2
chiragpoddar12 <chiragpoddar12@gmail.com>
2017-06-06 22:46:20 +0530

    Observable List for places

--- js/main.js
+++ js/main.js
@@ -4,3 +4,57 @@ function initMap() {
       center: {lat: -34.397, lng: 150.644}
     });
 }
+
+var Place = function(data){
+    this.name = ko.observable(data.name);
+    this.position = ko.observable(data.postion);
+}
+
+var initialPlaceList=[
+    {
+        name:'Mumbai',
+        position:{
+            lat : 19.0760,
+            lng : 72.8777
+        }
+    },
+    {
+        name:'Pune',
+        position : {
+            lat : 18.5204,
+            lng : 73.8567
+        }
+    },
+    {
+        name:'Goa',
+        position : {
+            lat : 15.2993,
+            lng : 74.1240
+        }
+    },
+    {
+        name:'Kolhapur',
+        position : {
+            lat : 16.7050,
+            lng : 74.2433
+        }
+    },
+    {
+        name: 'Ahmedabad',
+        position:{
+            lat : 23.0225,
+            lng : 72.5714
+        }
+    }
+
+];
+
+var ViewModel = function(){
+    var self = this;
+    this.placeList = ko.observableArray([]);
+
+    initialPlaceList.forEach(function(place){
+        this.placeList.push(new Place(place));
+    }, this);
+}
+ko.applyBindings(new ViewModel());
